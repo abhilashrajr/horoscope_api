b8299d78253105b64ac20a82fc05a54a884ee723
Gopinathibz <gopinath.ibz@gmail.com>
2016-12-02 18:30:54 +0530

    URL changed

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      post '/', to: 'home#index'
+      post '/horoscope', to: 'home#index'
     end
   end
 
