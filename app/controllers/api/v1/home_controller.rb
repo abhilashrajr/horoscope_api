b392219ef6403cde4eb97055ad8cc02596e7560e
Gopinathibz <gopinath.ibz@gmail.com>
2016-12-02 18:23:22 +0530

    Horoscope API created

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,16 +1,41 @@
 module Api
   module V1
     class HomeController < ApplicationController
+
       def index
-        p "!!!!!!!!!!!!!!!!!"
-        
-        h = Horoscope::Horo.new(
-          :datetime => Time.utc(params[:date_time].to_i), 
-          :zone => params[:zone].to_i,
-          :lat => params[:lat].to_i, :lon => params[:lon].to_i)
-        data = h.compute
-        render json: {result: true, data: data}
+        result = { result: false }
+        if params[:date].present? && params[:time].present?
+          begin
+            horoscope = find_horoscope(horoscope_params)
+            data = horoscope.compute
+            result[:result] = !horoscope.errors.present?
+            result[:data] = data
+          rescue Exception => e
+            result[:data] = "Please check the input format"
+          end
+        else
+          result[:data] = "date and time fields are mandatory"
+        end
+        render json: result
       end
+
+
+
+      private
+
+      def find_horoscope(options={})
+        date = options[:date].split('/')
+        time = options[:time].split(':')
+        h = Horoscope::Horo.new(
+          :datetime => Time.utc(date[0].to_i, date[1].to_i, date[2].to_i, time[0].to_i, time[1].to_i),
+          :zone     => options[:zone].to_i,
+          :lat      => options[:lat].to_i, :lon => options[:lon].to_i)
+      end
+
+      def horoscope_params
+        params.permit(:date, :time, :zone, :lat, :lon)
+      end
+
     end
   end
 end
